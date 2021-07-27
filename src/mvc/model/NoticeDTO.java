badfb06c88143677a87b0b8ec220fbec8ac1ec3c
SOYEON <78927@naver.com>
2021-07-27 20:30:57 +0900

    2021-07-27 공지사항 DTO 클래스 작성

---
+++ src/mvc/model/NoticeDTO.java
@@ -0,0 +1,55 @@
+package mvc.model;
+
+public class NoticeDTO {
+	
+	private int num;		// 순번
+	private String id;		// 작성자 아이디
+	private String name;	// 작성자 이름
+	private String title;	// 게시물 제목
+	private String content;	// 게시물 내용
+	private String date;	// 등록일
+	private int hit;		// 조회수
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+}
