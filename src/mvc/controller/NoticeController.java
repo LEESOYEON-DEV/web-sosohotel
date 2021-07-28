2f1f3f12cea1f292340a4381bae2904f89bd7e3f
SOYEON <78927@naver.com>
2021-07-28 22:28:01 +0900

    2021-07-28 메소드 기본 필드, 설정 추가

--- src/mvc/controller/NoticeController.java
+++ src/mvc/controller/NoticeController.java
@@ -15,5 +15,14 @@ public class NoticeController extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		String requestURI = req.getRequestURI(); // 웹 브라우저가 요청한 URI 경로 (ex. /web_sosohotel/page/testPage.jsp)
+		String contextPath = req.getContextPath(); // 웹 애플리케이션 콘텍스트 경로 (ex. /web_sosohotel)
+		String command = requestURI.substring(contextPath.length()); // requestURI에서 contextPath 이후 글자부터 출력 (ex. /page/testPage.jsp)
+		
+		req.setCharacterEncoding("UTF-8"); // 웹 브라우저에 응답할 문자 인코딩 설정
+		resp.setContentType("text/html; charset=UTF-8"); // 웹 브라우저에 응답할 MIME 유형 설정
+		
+		
+		
 	}
 }
