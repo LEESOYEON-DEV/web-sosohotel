bbdc5a724970bf624634e1209725e8db64662212
SOYEON <78927@naver.com>
2021-08-07 20:36:53 +0900

    2021-08-07 보유객실수, 잔여객실수 필드 삭제

--- src/mvc/model/RoomCounterDTO.java
+++ src/mvc/model/RoomCounterDTO.java
@@ -5,9 +5,7 @@ public class RoomCounterDTO {
 	private String code;	// 구분
 	private String checkIn;	// 체크인
 	private String type;	// 객실타입
-	private int num;		// 보유객실수
 	private int count;		// 예약객실수
-	private int remained;	// 잔여객실수
 	
 	public String getCode() {
 		return code;
@@ -27,22 +25,10 @@ public class RoomCounterDTO {
 	public void setType(String type) {
 		this.type = type;
 	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
 	public int getCount() {
 		return count;
 	}
 	public void setCount(int count) {
 		this.count = count;
-	}
-	public int getRemained() {
-		return remained;
-	}
-	public void setRemained(int remained) {
-		this.remained = remained;
 	}
 }
