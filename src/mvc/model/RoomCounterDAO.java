f4703ffbc3c8fbd537c4d30635ab284c4398c179
SOYEON <78927@naver.com>
2021-08-07 21:18:28 +0900

    2021-08-07 동일 날짜, 동일 객실 데이터가 있다면 기존 데이터의 예약 객실 수를 증가시키는 기능 추가

--- src/mvc/model/RoomCounterDAO.java
+++ src/mvc/model/RoomCounterDAO.java
@@ -26,23 +26,39 @@ public class RoomCounterDAO {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
-		String sql = "INSERT INTO room_counter(rm_code, check_in, rm_type, rm_count) VALUES(?, ?, ?, ?)";
+		String sql = "SELECT rm_count FROM room_counter WHERE rm_code=?";
 		
 		try {
 			conn = DBConn.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, dto.getCode());
-			pstmt.setString(2, dto.getCheckIn());
-			pstmt.setString(3, dto.getType());
-			pstmt.setInt(4, dto.getCount());
-			pstmt.executeUpdate();			
+			
+			int count = 0;
+			if(rs.next()) {
+				count = rs.getInt("rm_count") + dto.getCount();
+				sql = "UPDATE room_counter SET rm_count=? WHERE rm_code=?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, count);
+				pstmt.setString(2, dto.getCode());
+				pstmt.executeUpdate();
+			} else {
+				sql = "INSERT INTO room_counter(rm_code, check_in, rm_type, rm_count) VALUES(?, ?, ?, ?)";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, dto.getCode());
+				pstmt.setString(2, dto.getCheckIn());
+				pstmt.setString(3, dto.getType());
+				pstmt.setInt(4, dto.getCount());
+				pstmt.executeUpdate();
+			}
 			
 		} catch(Exception e) {
 			System.out.println("insertRoomCounter() error : " + e);
 			
 		} finally {
 			try {
+				if(rs != null) rs.close();
 				if(pstmt != null) pstmt.close();
 				if(conn != null) conn.close();
 				
